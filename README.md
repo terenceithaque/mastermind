b9d9ea6954298aca3921df58e0275640f3b20344
terenceithaque <terence.lethierry@protonmail.com>
2023-09-09 10:29:59 +0200

    Gestion de la fin de partie et du game over + ajout de README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+Une version du jeu du mastermind en ligne de commande écrite avec Python.
--- main.py
+++ main.py
@@ -5,38 +5,92 @@ from random import randrange  # Importation de la fonction randrange du module r
 def choisir_couleurs(couleurs_possibles):
     "Choisir 5 couleurs à partir d'une liste de couleurs possibles"
     couleurs_choisies = []  # Liste dans laquelle on stockera toutes les couleurs choisies
-    i = 0
-    while i < 5:
+    n_couleurs_choisies = 0  # Nombre de couleurs choisies
+    while n_couleurs_choisies < 5:
         # Choisir une couleur au hasard
         couleur = randrange(len(couleurs_possibles))
         couleur = couleurs_possibles[couleur]
         couleurs_choisies.append(couleur)
-        i += 1
+        n_couleurs_choisies += 1
 
     return couleurs_choisies
 
 
-def comparer_couleurs(liste1, liste2):
-    "Comparer les couleurs contenues dans 2 listes différentes. Cela va nous servir pour comparer les couleurs entrée par le joueur et les couleurs choisies."
-    # Liste dans laquelle on stockera des booléens qui seront True si une couleur est commune aux deux listes, et False si une couleur n'est pas commune aux deux listes.
-    couleurs_communes = []
-    est_commune = False  # Est-ce qu'une couleur est commune aux deux listes ?
-
+def couleurs_communes(liste1, liste2):
+    "Trouver les couleurs communes de deux listes différentes"
+    couleurs_com = []  # Liste dans laquelle on contiendra toute couleur commune aux deux listes
     for couleur in liste1:  # Pour chaque couleur de la première liste
-        if couleur in liste2:  # Si la couleur est également présente dans la deuxième liste
-            est_commune = True  # Alors la couleur est commune aux deux listes
-            couleurs_communes.append(est_commune)
+        if couleur in liste2:
+            couleurs_com.append(couleur)
+
+    return couleurs_com
+
+
+def comparer_positions(liste1, liste2):
+    "Comparer les positions des couleurs dans deux listes différentes"
+    # Trouver toutes les couleurs communes aux deux listes
+    couleurs_com = couleurs_communes(liste1, liste2)
+   # print("couleurs_com :", couleurs_com)
+    # Booléen pour vérifier si la position d'une couleur est la même d'une liste à l'autre
+    pos_identique = True
+    pos = []  # Liste pour contenir les valeurs de pos_identique pour chaque couleur
+    for couleur in couleurs_com:
+        if couleur in liste1 and couleur in liste2:
+            # print("Position liste 1 :", liste1.index(couleur),
+            # "Position liste 2 :", liste2.index(couleur))
+
+            if liste1.index(couleur) != liste2.index(couleur):
+                # print("La position des couleurs est différente !")
+                pos_identique = False
+                pos.append(pos_identique)
+
+            else:
+                pos_identique = True
+                pos.append(pos_identique)
+
+    return pos
+
+
+def jeu():
+    "Jeu"
+    # Liste des couleurs que le jeu peut tirer au hasard
+    couleurs_possibles = ["blanc", "bleu", "vert", "noir", "blanc", "rouge"]
+    couleurs_choisies = choisir_couleurs(couleurs_possibles)
+    print(couleurs_choisies)
+    essais = 12
+
+    while essais > 0:
+        # Demander au joueur de saisir 5 couleurs
+        print()  # Sauter à la ligne
+        couleurs = input("Saisissez 5 couleurs:")
+        couleurs = couleurs.split()  # Représenter la saisie du joueur sous forme de liste
+        # Comparer les positions des couleurs de couleurs et couleurs_choisies
+        comp = comparer_positions(couleurs, couleurs_choisies)
+        # Pour chaque couleur entrée par le joueur
+        for i, couleur in enumerate(couleurs):
+            if comp[i]:  # Si la couleur est bien placée
+                print(couleur, ": O, ", end=" ")
+            else:
+                print(couleur, ": X, ", end=" ")
+
+            if couleur not in couleurs_choisies:
+                print(couleur)
+
+        if couleurs != couleurs_choisies:  # Si le joueur n'a pas deviné les bonnes couleurs et/ou leur position
+            essais -= 1
+            print("Essais restants :", essais)
+
+        if couleurs == couleurs_choisies:  # Si le joueur a deviné toutes les couleurs ainsi que leur position
+            # Afficher un message de fin de partie
+            print("Bravo ! Vous avez gagné !")
+            break  # Terminer la partie
+
+        if essais > 0:
+            continue
 
         else:
-            est_commune = False  # La couleur n'est pas commune aux deux listes
-            couleurs_communes.append(est_commune)
-
-        est_commune = False  # Réinitialiser la variable est_commune
-
-    return couleurs_communes
+            print()  # Sauter à la ligne
+            print("GAME OVER")
 
 
-liste1 = ["bleu", "rouge", "vert", "blanc", "violet"]
-liste2 = ["orange", "noir", "bleu", "vert", "marron"]
-
-print(comparer_couleurs(liste1, liste2))
+jeu()
